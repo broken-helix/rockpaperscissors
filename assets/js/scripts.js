bcf3850d20d9dab9a17018f5ed16e54b16e6dcbf
James Evans <james_e75@hotmail.com>
2022-12-27 20:59:44 +0000

    Add functionality to displayUserChoice function and add to runGame function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,20 +4,27 @@ for (let button of buttons) {
     button.addEventListener("click", function() {
         let gameType = this.getAttribute("id");
         runGame(gameType);
+        console.log(gameType);
     });
 }
 
 function runGame(gameType) {
-    
+    displayUserChoice(gameType);
 }
 
 
 function displayUserChoice(gameType) {
-    
+    if (gameType === "user-rock") {
+        document.getElementById("user-choice").innerHTML = '<i class="fa-regular fa-hand-back-fist"></i>';
+    } else if (gameType === "user-paper") {
+        document.getElementById("user-choice").innerHTML = '<i class="fa-regular fa-hand"></i>';
+    } else if (gameType === "user-scissors") {
+        document.getElementById("user-choice").innerHTML = '<i class="fa-regular fa-hand-scissors"></i>';
+    }
 }
 
 function calculateComputerChoice() {
-
+    
 }
 
 function compareChoices() {
