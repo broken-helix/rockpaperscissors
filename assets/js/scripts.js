244eb9f8ac37c2a20b955427dae1fc16a6b4e7a5
James Evans <james_e75@hotmail.com>
2023-01-04 10:02:05 +0000

    Add functionality to increment scores

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,13 +6,43 @@ for (let button of buttons) {
         let computerChoice = calculateComputerChoice();
         runGame(gameType, computerChoice);
     });
-}
+};
+
+function displayUserChoice(gameType) {
+    if (gameType === "rock") {
+        document.getElementById("user-choice").innerHTML = '<i class="fa-regular fa-hand-back-fist"></i>';
+    } else if (gameType === "paper") {
+        document.getElementById("user-choice").innerHTML = '<i class="fa-regular fa-hand"></i>';
+    } else if (gameType === "scissors") {
+        document.getElementById("user-choice").innerHTML = '<i class="fa-regular fa-hand-scissors"></i>';
+    }
+};
+
+function calculateComputerChoice(computerChoice) {
+
+    computerChoice = Math.floor(Math.random() * 3);
+
+    if (computerChoice === 0) {
+        document.getElementById("computer-choice").innerHTML = '<i class="fa-regular fa-hand-back-fist"></i>';
+        computerChoice = "rock";
+
+    } else if (computerChoice === 1) {
+        document.getElementById("computer-choice").innerHTML = '<i class="fa-regular fa-hand"></i>';
+        computerChoice = "paper";
+
+    } else if (computerChoice === 2) {
+        document.getElementById("computer-choice").innerHTML = '<i class="fa-regular fa-hand-scissors"></i>';
+        computerChoice = "scissors";
+    }
+
+    return computerChoice;
+};
 
 function runGame(gameType, computerChoice) {
-    
+
     displayUserChoice(gameType);
 
-    if (gameType === compChoice) {
+    if (gameType === computerChoice) {
         document.getElementById("win-state").innerText = "DRAW";
     } else if (gameType === "rock" && computerChoice === "paper") {
         document.getElementById("win-state").innerText = "LOSE";
@@ -27,43 +57,22 @@ function runGame(gameType, computerChoice) {
     } else if (gameType === "scissors" && computerChoice === "paper") {
         document.getElementById("win-state").innerText = "WIN";
     } 
-}
-
-
-function displayUserChoice(gameType) {
-    if (gameType === "rock") {
-        document.getElementById("user-choice").innerHTML = '<i class="fa-regular fa-hand-back-fist"></i>';
-    } else if (gameType === "paper") {
-        document.getElementById("user-choice").innerHTML = '<i class="fa-regular fa-hand"></i>';
-    } else if (gameType === "scissors") {
-        document.getElementById("user-choice").innerHTML = '<i class="fa-regular fa-hand-scissors"></i>';
-    }
-}
-
-function calculateComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3);
-    console.log(computerChoice);
-    if (computerChoice === 0) {
-        document.getElementById("computer-choice").innerHTML = '<i class="fa-regular fa-hand-back-fist"></i>';
-        computerChoice = "rock";
-    } else if (computerChoice === 1) {
-        document.getElementById("computer-choice").innerHTML = '<i class="fa-regular fa-hand"></i>';
-        computerChoice = "paper";
-    } else if (computerChoice === 2) {
-        document.getElementById("computer-choice").innerHTML = '<i class="fa-regular fa-hand-scissors"></i>';
-        computerChoice = "scissors";
-    }
-
-}
-
-function compareChoices() {
-
-}
+    
+    incrementScores();
+};
 
 function displayWinner() {
 
-}
+};
 
 function incrementScores() {
+    let userScore = parseInt(document.getElementById("user-score").innerText);
+    let computerScore = parseInt(document.getElementById("computer-score").innerText);
+    let winState = document.getElementById("win-state");
 
-}
+    if (winState.innerText === "WIN") {
+        document.getElementById("user-score").innerText = ++userScore;
+    } else if (winState.innerText === "LOSE") {
+        document.getElementById("computer-score").innerText = ++computerScore;
+    } 
+};
